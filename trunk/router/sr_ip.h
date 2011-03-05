41bb0d7c65ff2ee313dbcfee6b3b9f1c0f4dadad
cwoodruf@gmail.com <cwoodruf@gmail.com@429fb507-ded1-ea00-d9f1-d3ff818314a8>
2011-03-05 19:51:00 +0000

    the timeout version of traceroute is working

--- trunk/router/sr_ip.h
+++ trunk/router/sr_ip.h
@@ -2,11 +2,12 @@
 #define SR_IP_H
 
 /** icmp types */
-#define ICMP_ECHO_REPLY 0
-#define ICMP_UNREACHABLE 3
-#define ICMP_ECHO_REQUEST 8
-#define ICMP_TIME_EXCEEDED 11
-#define ICMP_TRACEROUTE 30
+#define ICMP_ECHO_REPLY 0x00
+#define ICMP_UNREACHABLE 0x03
+#define ICMP_PORT_UNAVAILABLE 0x03
+#define ICMP_ECHO_REQUEST 0x08
+#define ICMP_TIME_EXCEEDED 0x0b
+#define ICMP_TRACEROUTE 0x1e
 
 /** recommended here: http://www.maxi-pedia.com/time+to+live */
 #define IP_MAX_HOPS 128
@@ -18,39 +19,56 @@
 #define IPPROTO_ICMP 0x0001  
 #endif
 
-/** minimal icmp header: http://www.networksorcery.com/enp/protocol/icmp.htm */
+#ifndef IPPROTO_TCP
+#define IPPROTO_TCP 0x0006  
+#endif
+
+#ifndef IPPROTO_UDP
+#define IPPROTO_UDP 0x0011
+#endif
+
+#define IPDATASIZE (VNSCMDSIZE-sizeof(struct sr_ethernet_hdr)-sizeof(struct ip))
+
+/** strategy: make one big data structure that can be overlaid on a packet */
+
+/** icmp structs - these are all 8 bytes */
 struct sr_icmp_timeout
 {
-	uint8_t type;
-	uint8_t code;
-	uint16_t checksum;
 	uint32_t unused;
 } __attribute__ ((packed)) ;
 
 struct sr_icmp_unreachable
 {
-	uint8_t type;
-	uint8_t code;
-	uint16_t checksum;
 	uint16_t unused;
 	uint16_t mtu;
 } __attribute__ ((packed)) ;
 
 struct sr_icmp_echo_reply
 {
-	uint8_t type;
-	uint8_t code;
-	uint16_t checksum;
 	uint16_t id;
 	uint16_t sequence;
 } __attribute__ ((packed)) ;
 
-#ifndef IPPROTO_TCP
-#define IPPROTO_TCP 0x0006  
-#endif
+/** put all icmp structs together */
+union sr_icmp_fields 
+{
+	struct sr_icmp_timeout timeout;
+	struct sr_icmp_unreachable nothere;
+	struct sr_icmp_echo_reply ping;
+} __attribute__ ((packed)) ;
 
-/** minimal tcp header: http://www.networksorcery.com/enp/protocol/tcp.htm */
-struct sr_tcp
+struct sr_icmp
+{
+	uint8_t type;
+	uint8_t code;
+	uint16_t checksum;
+	union sr_icmp_fields fields;
+	uint8_t data[IPDATASIZE-8];
+} __attribute__ ((packed)) ;
+/** end icmp stuff */
+
+/** udp and tcp base headers */
+struct sr_tcp /** 20 bytes */
 {
 	uint16_t src_port;
 	uint16_t dest_port;
@@ -60,20 +78,35 @@ struct sr_tcp
 	uint16_t window;
 	uint16_t checksum;
 	uint16_t urgent;
+	uint8_t data[IPDATASIZE-20];
 } __attribute ((packed)) ;
 
-#ifndef IPPROTO_UDP
-#define IPPROTO_UDP 0x0011
-#endif
-
-/** minimal udp header: http://www.networksorcery.com/enp/protocol/udp.htm */
-struct sr_udp 
+struct sr_udp /** 8 bytes */
 {
 	uint16_t src_port;
 	uint16_t dest_port;
 	uint16_t len;
 	uint16_t checksum;
+	uint8_t data[IPDATASIZE-8];
 } __attribute__ ((packed)) ;
+/** end udp and tcp */
+
+/** combine all the different types of payload together */
+union sr_ip_payload
+{
+	struct sr_tcp tcp;
+	struct sr_udp udp;
+	struct sr_icmp icmp;
+} __attribute ((packed)) ;
+
+/** the big data structure */
+struct sr_ip_packet
+{
+	struct sr_ethernet_hdr eth;
+	struct ip ip;
+	union sr_ip_payload d;
+} __attribute ((packed)) ;
+/** end cooked ip packet structures */
 
 /** both udp and tcp use this pseudoheader to computer checksums; reserved = 0 always */
 struct sr_pseudoheader {
