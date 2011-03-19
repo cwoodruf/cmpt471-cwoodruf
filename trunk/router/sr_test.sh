f8041ba80a1cda2423401531b33ca12080853f59
cwoodruf@gmail.com <cwoodruf@gmail.com@429fb507-ded1-ea00-d9f1-d3ff818314a8>
2011-03-19 22:14:38 +0000

    update sr_test.sh to do grading sheet tests

--- trunk/router/sr_test.sh
+++ trunk/router/sr_test.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
+ipbase=171.67.245
 /usr/bin/killall sr
-srpid=`/bin/pidof sr`
-if [ "$srpid" == "" ] 
-then
-	echo starting sr
-	./sr_start.sh
-	/bin/sleep 5
-	echo double ping ARP test
-	/bin/ping -c 3 171.67.245.103
-	/bin/ping -c 3 171.67.245.103
-fi
+make clean
+make
 
-echo starting background pings \(stress test\)
-/bin/ping -q 171.67.245.96 &
-/bin/ping -q 171.67.245.100 &
-/bin/ping -q 171.67.245.101 &
-/bin/ping -q 171.67.245.102 &
-/bin/ping -q 171.67.245.103 &
+echo starting sr
+./sr_start.sh
+/bin/sleep 5
+echo double ping ARP test
+/bin/ping -c 3 171.67.245.103
+/bin/ping -c 3 171.67.245.103
 
-echo tracepaths
-/usr/bin/tracepath -n 171.67.245.96
-/usr/bin/tracepath -n 171.67.245.100
-/usr/bin/tracepath -n 171.67.245.101
-/usr/bin/tracepath -n 171.67.245.102
-/usr/bin/tracepath -n 171.67.245.103
-
-echo downloading big.jpg from both interfaces
-while true 
+for ip in 96 100 101 102 103
 do
-/usr/bin/wget --timeout=5 --no-verbose -O ~/Desktop/big_101.jpg http://171.67.245.101/big.jpg 
-/usr/bin/wget --timeout=5 --no-verbose -O ~/Desktop/big_103.jpg http://171.67.245.103/big.jpg 
+	/bin/ping -c 5 $ipbase.$ip 
 done
+
+for ip in 101 103  
+do 
+	/usr/bin/tracepath -n $ipbase.$ip
+	/usr/bin/firefox http://$ipbase.$ip/big.jpg &
+done
+
