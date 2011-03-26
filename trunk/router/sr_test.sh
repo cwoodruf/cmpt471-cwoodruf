f0195d1f33ced3b7fbe84985e558f51ca1266898
cwoodruf@gmail.com <cwoodruf@gmail.com@429fb507-ded1-ea00-d9f1-d3ff818314a8>
2011-03-26 00:02:34 +0000

    changed tests to match those of the assignment

--- trunk/router/sr_test.sh
+++ trunk/router/sr_test.sh
@@ -7,18 +7,22 @@ make
 echo starting sr
 ./sr_start.sh
 /bin/sleep 5
-echo double ping ARP test
-/bin/ping -c 3 171.67.245.103
-/bin/ping -c 3 171.67.245.103
-
 for ip in 96 100 101 102 103
 do
 	/bin/ping -c 5 $ipbase.$ip 
 done
 
-for ip in 101 103  
+echo let arp time out
+sleep 16
+
+echo double ping ARP test
+/bin/ping -c 3 171.67.245.103
+/bin/ping -c 3 171.67.245.103
+
+for ip in 101 103 96
 do 
+	echo tracepath $ipbase.$ip
 	/usr/bin/tracepath -n $ipbase.$ip
-	/usr/bin/firefox http://$ipbase.$ip/big.jpg &
+#	/usr/bin/firefox http://$ipbase.$ip/big.jpg &
 done
 
